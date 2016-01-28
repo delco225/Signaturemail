277ff214bc4eb32290e31a086ec6d261fa331c2b
Ahoussi say <Ahoussi say>
2016-01-28 17:30:13 -0500

    creation of the source code with Eclips Mars

---
+++ MailSignature/src/sgnature/core/Principal.java
@@ -0,0 +1,34 @@
+/**
+ * @author Ahoussi Armand 
+ * 
+ * e-mail delcolink@gmail.com 
+ * this is the general class that should run the programm 
+ */
+
+package sgnature.core;
+
+public class Principal {
+
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+
+	}
+
+	
+	
+	
+	
+	
+	
+}
