3e5903b3cebb45102c7c6c253628ea4de6b69b15
Ahoussi say <Ahoussi say>
2016-02-03 17:19:41 -0500

    test

---
+++ MailSignature/src/src/sgnature/core/Repository.java
@@ -0,0 +1,19 @@
+package src.sgnature.core;
+
+public class Repository {
+
+	public static void main(String[] args) {
+		
+		while (true){
+			
+		System.out.println("this is a test on The server path ") ; 	
+			
+			
+		}
+		
+		
+		
+
+	}
+
+}
